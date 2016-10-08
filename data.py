bc2a8f9c4880f767890be16542f861ef0839c4e4
Nefermu <onuron@gmail.com>
2016-10-08 20:21:45 +0300

    first

---
+++ data.py
@@ -0,0 +1,39 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+classifiers = [
+    KNeighborsClassifier(),
+    SVC(),
+    GaussianProcessClassifier(),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    MLPClassifier(),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()]
+
+#
+
+
+#[height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+
+
+for cl in classifiers:
+	clf = cl.fit(X,Y)
+	prediction =clf.predict([[177,75,42]])
+	print prediction
+
+
+
+
